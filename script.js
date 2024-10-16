cdf09221de6e52f9cb3e348a6d875ea3ee39d8a6
JavaChrist <97617119+JavaChrist@users.noreply.github.com>
2024-10-16 04:17:31 +0200

    First commit new JavaChrist

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const textElement = document.querySelector('.home-content .text');
+
+    if (textElement) {
+        const options = {
+            strings: ['Développeur Web', 'Designer UI/UX', 'Freelance'],
+            typeSpeed: 50,
+            backSpeed: 25,
+            backDelay: 2000,
+            loop: true,
+        };
+
+        const observer = new IntersectionObserver(function (entries) {
+            if (entries[0].isIntersecting) {
+                new Typed(textElement, options);
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(textElement);
+    }
+});
